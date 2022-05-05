842726f5d16cbc2769e52d02d869730970fc5a51
Samir Diaz <samir.diaz.1206@gmail.com>
2022-05-05 17:13:19 -0500

    se agrego form en index para probar export

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,19 @@
 export default () => {
+
+	const submit = e => {
+		e.preventDefault();
+
+		console.log(e.target[0]);
+		console.log(e.target[1]);
+	}
+
 	return (
-		<h1>Index</h1>
+		<form onSubmit={submit}>
+			<input type="text" />
+			<input type="password" />
+			<button>
+				Submit
+			</button>
+		</form>
 	);
 }
